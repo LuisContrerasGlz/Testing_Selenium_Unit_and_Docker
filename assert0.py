f59016264bbef4d9cf974e56c9f712c90d434912
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2023-05-14 02:40:25 -0600

    adding comments

--- assert0.py
+++ assert0.py
@@ -1,5 +1,4 @@
 def square(x):
-    return x * x
+    return x * x  # Return the square of the input x
 
-
-assert square(10) == 100
+assert square(10) == 100  # Assert that the square of 10 is equal to 100
