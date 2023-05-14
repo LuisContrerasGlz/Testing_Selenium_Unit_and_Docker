bdfaca67a802493adb557da4b6cabda1a7311112
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2023-05-14 02:38:23 -0600

    adding comments

--- tests0.py
+++ tests0.py
@@ -1,6 +1,6 @@
-from prime import is_prime
+from prime import is_prime  # Import the is_prime function from the prime module
 
 
-def test_prime(n, expected):
-    if is_prime(n) != expected:
-        print(f"ERROR on is_prime({n}), expected {expected}")
+def test_prime(n, expected):  # Define a function that takes a number and an expected value
+    if is_prime(n) != expected:  # Check if the result of is_prime(n) matches the expected value
+        print(f"ERROR on is_prime({n}), expected {expected}")  # If the expected value is not matched, print an error message
